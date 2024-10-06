1c45204466fc590644c81f29c29d467151a6c2c0
JiYiqaq <macdonaldchloe794@gmail.com>
2024-10-06 16:45:39 +0800

    无

--- books.py
+++
@@ -1,7 +0,0 @@
-import requests
-respone = requests.get('https://books.toscrape.com/')
-if respone.ok:
-    with open('books.txt', 'w',encoding="utf-8") as f:
-        f.write(respone.text)
-else:
-    print(respone)
